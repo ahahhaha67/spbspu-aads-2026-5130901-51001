5fc712635c87123893a2c7ed8258e219b7588fad
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-05-18 17:05:15 +0300

    feat(stack): push with grow

--- krivoshapov.ivan/S2/stack.hpp
+++ krivoshapov.ivan/S2/stack.hpp
@@ -22,6 +22,20 @@ namespace krivoshapov
       deallocate(data_);
     }
 
+    void push(const T &value)
+    {
+      ensure();
+      data_[size_] = value;
+      ++size_;
+    }
+
+    void push(T &&value)
+    {
+      ensure();
+      data_[size_] = std::move(value);
+      ++size_;
+    }
+
   private:
     T *data_;
     size_t size_;
@@ -29,6 +43,31 @@ namespace krivoshapov
 
     static T *allocate(size_t n) { return new T[n](); }
     static void deallocate(T *p) noexcept { delete[] p; }
+
+    void ensure()
+    {
+      if (size_ < cap_)
+      {
+        return;
+      }
+      size_t ncap = (cap_ == 0) ? 8 : cap_ * 2;
+      T *buf = allocate(ncap);
+      try
+      {
+        for (size_t i = 0; i < size_; ++i)
+        {
+          buf[i] = std::move_if_noexcept(data_[i]);
+        }
+      }
+      catch (...)
+      {
+        deallocate(buf);
+        throw;
+      }
+      deallocate(data_);
+      data_ = buf;
+      cap_ = ncap;
+    }
   };
 }
 
