14979d5781d57d954ab892b711c904a2f9992584
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-05-18 17:17:41 +0300

    calc.hpp

---
+++ krivoshapov.ivan/S2/calc.hpp
@@ -0,0 +1,17 @@
+#ifndef KRIVOSHAPOV_CALC_HPP
+#define KRIVOSHAPOV_CALC_HPP
+
+#include <cstddef>
+#include <string>
+#include <stdexcept>
+#include "stack.hpp"
+#include "queue.hpp"
+
+namespace krivoshapov
+{
+  size_t evaluate(const std::string &line);
+
+  bool is_blank(const std::string &line);
+}
+
+#endif
