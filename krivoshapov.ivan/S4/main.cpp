8bb5808a43fe0c0d80d739849e0cbc07df9d90ed
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-05-22 16:18:25 +0300

    feat(main): add main

--- krivoshapov.ivan/S4/main.cpp
+++ krivoshapov.ivan/S4/main.cpp
@@ -1,5 +1,31 @@
+#include <exception>
+#include <fstream>
 #include <iostream>
+#include "commands.hpp"
 
-int main(){
-
+int main(int argc, char **argv)
+{
+  if (argc < 2)
+  {
+    std::cerr << "ERROR: filename argument required\n";
+    return 1;
+  }
+  std::ifstream file(argv[1]);
+  if (!file.is_open())
+  {
+    std::cerr << "ERROR: cannot open file\n";
+    return 1;
+  }
+  krivoshapov::DatasetStore store(16);
+  try
+  {
+    krivoshapov::loadDatasets(file, store);
+    krivoshapov::runCommands(std::cin, std::cout, store);
+  }
+  catch (const std::exception &e)
+  {
+    std::cerr << "ERROR: " << e.what() << "\n";
+    return 1;
+  }
+  return 0;
 }
